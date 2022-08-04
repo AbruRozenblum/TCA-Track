9992c35b3021a4314c5ef4af651d42346c0dd1c0
AbruRozenblum <110607863+AbruRozenblum@users.noreply.github.com>
2022-08-04 14:19:56 -0300

    casi listo

--- Pantalla 1 Registro/Form1.Designer.cs
+++ Pantalla 1 Registro/Form1.Designer.cs
@@ -40,44 +40,61 @@
             this.cmbDía = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             this.SuspendLayout();
             // 
             // txtMailDelUsuario
             // 
-            this.txtMailDelUsuario.Location = new System.Drawing.Point(288, 129);
+            this.txtMailDelUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMailDelUsuario.Location = new System.Drawing.Point(538, 170);
             this.txtMailDelUsuario.Name = "txtMailDelUsuario";
-            this.txtMailDelUsuario.Size = new System.Drawing.Size(267, 20);
+            this.txtMailDelUsuario.Size = new System.Drawing.Size(315, 26);
             this.txtMailDelUsuario.TabIndex = 4;
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(288, 185);
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtNombre.Location = new System.Drawing.Point(538, 238);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(267, 20);
+            this.txtNombre.Size = new System.Drawing.Size(315, 26);
             this.txtNombre.TabIndex = 5;
             // 
             // txtMailEspecialista
             // 
-            this.txtMailEspecialista.Location = new System.Drawing.Point(288, 242);
+            this.txtMailEspecialista.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMailEspecialista.Location = new System.Drawing.Point(538, 306);
             this.txtMailEspecialista.Name = "txtMailEspecialista";
-            this.txtMailEspecialista.Size = new System.Drawing.Size(267, 20);
+            this.txtMailEspecialista.Size = new System.Drawing.Size(315, 26);
             this.txtMailEspecialista.TabIndex = 6;
             // 
             // cmbDiagnostico
             // 
+            this.cmbDiagnostico.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.cmbDiagnostico.FormattingEnabled = true;
-            this.cmbDiagnostico.Location = new System.Drawing.Point(288, 353);
+            this.cmbDiagnostico.Location = new System.Drawing.Point(538, 451);
             this.cmbDiagnostico.Name = "cmbDiagnostico";
-            this.cmbDiagnostico.Size = new System.Drawing.Size(267, 21);
+            this.cmbDiagnostico.Size = new System.Drawing.Size(312, 28);
             this.cmbDiagnostico.TabIndex = 7;
             // 
             // rbtnFemenino
             // 
             this.rbtnFemenino.AutoSize = true;
-            this.rbtnFemenino.Location = new System.Drawing.Point(288, 402);
+            this.rbtnFemenino.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rbtnFemenino.Location = new System.Drawing.Point(544, 521);
             this.rbtnFemenino.Name = "rbtnFemenino";
-            this.rbtnFemenino.Size = new System.Drawing.Size(71, 17);
+            this.rbtnFemenino.Size = new System.Drawing.Size(98, 24);
             this.rbtnFemenino.TabIndex = 8;
             this.rbtnFemenino.TabStop = true;
             this.rbtnFemenino.Text = "Femenino";
@@ -86,9 +103,10 @@
             // rbtnnMasculino
             // 
             this.rbtnnMasculino.AutoSize = true;
-            this.rbtnnMasculino.Location = new System.Drawing.Point(365, 402);
+            this.rbtnnMasculino.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rbtnnMasculino.Location = new System.Drawing.Point(648, 521);
             this.rbtnnMasculino.Name = "rbtnnMasculino";
-            this.rbtnnMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rbtnnMasculino.Size = new System.Drawing.Size(98, 24);
             this.rbtnnMasculino.TabIndex = 9;
             this.rbtnnMasculino.TabStop = true;
             this.rbtnnMasculino.Text = "Masculino";
@@ -97,9 +115,10 @@
             // rbtnOtro
             // 
             this.rbtnOtro.AutoSize = true;
-            this.rbtnOtro.Location = new System.Drawing.Point(444, 402);
+            this.rbtnOtro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rbtnOtro.Location = new System.Drawing.Point(752, 521);
             this.rbtnOtro.Name = "rbtnOtro";
-            this.rbtnOtro.Size = new System.Drawing.Size(45, 17);
+            this.rbtnOtro.Size = new System.Drawing.Size(58, 24);
             this.rbtnOtro.TabIndex = 10;
             this.rbtnOtro.TabStop = true;
             this.rbtnOtro.Text = "Otro";
@@ -107,26 +126,29 @@
             // 
             // cmbMes
             // 
+            this.cmbMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.cmbMes.FormattingEnabled = true;
-            this.cmbMes.Location = new System.Drawing.Point(343, 296);
+            this.cmbMes.Location = new System.Drawing.Point(593, 381);
             this.cmbMes.Name = "cmbMes";
-            this.cmbMes.Size = new System.Drawing.Size(81, 21);
+            this.cmbMes.Size = new System.Drawing.Size(81, 26);
             this.cmbMes.TabIndex = 11;
             // 
             // cmbAño
             // 
+            this.cmbAño.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.cmbAño.FormattingEnabled = true;
-            this.cmbAño.Location = new System.Drawing.Point(430, 296);
+            this.cmbAño.Location = new System.Drawing.Point(680, 381);
             this.cmbAño.Name = "cmbAño";
-            this.cmbAño.Size = new System.Drawing.Size(55, 21);
+            this.cmbAño.Size = new System.Drawing.Size(55, 26);
             this.cmbAño.TabIndex = 12;
             // 
             // cmbDía
             // 
+            this.cmbDía.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.cmbDía.FormattingEnabled = true;
-            this.cmbDía.Location = new System.Drawing.Point(288, 296);
+            this.cmbDía.Location = new System.Drawing.Point(538, 381);
             this.cmbDía.Name = "cmbDía";
-            this.cmbDía.Size = new System.Drawing.Size(49, 21);
+            this.cmbDía.Size = new System.Drawing.Size(49, 26);
             this.cmbDía.TabIndex = 13;
             // 
             // button1
@@ -135,9 +157,9 @@
             this.button1.BackgroundImage = global::Pantalla_1_Registro.Properties.Resources.REGISTRARSE;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(352, 437);
+            this.button1.Location = new System.Drawing.Point(631, 588);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 28);
+            this.button1.Size = new System.Drawing.Size(139, 40);
             this.button1.TabIndex = 14;
             this.button1.UseVisualStyleBackColor = false;
             // 
@@ -147,18 +169,78 @@
             this.pictureBox1.BackgroundImage = global::Pantalla_1_Registro.Properties.Resources.BIENVENIDOS_1;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.pictureBox1.Location = new System.Drawing.Point(181, 24);
+            this.pictureBox1.Location = new System.Drawing.Point(450, 34);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(482, 38);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DarkBlue;
+            this.pictureBox2.Location = new System.Drawing.Point(538, 202);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(315, 30);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.DarkBlue;
+            this.pictureBox3.Location = new System.Drawing.Point(538, 134);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(315, 30);
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.DarkBlue;
+            this.pictureBox4.Location = new System.Drawing.Point(538, 270);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(315, 30);
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.DarkBlue;
+            this.pictureBox5.Location = new System.Drawing.Point(538, 338);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(315, 30);
+            this.pictureBox5.TabIndex = 18;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.DarkBlue;
+            this.pictureBox6.Location = new System.Drawing.Point(538, 413);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(315, 30);
+            this.pictureBox6.TabIndex = 19;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.DarkBlue;
+            this.pictureBox7.Location = new System.Drawing.Point(538, 485);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(315, 30);
+            this.pictureBox7.TabIndex = 20;
+            this.pictureBox7.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkOrchid;
-            this.ClientSize = new System.Drawing.Size(847, 520);
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.cmbDía);
             this.Controls.Add(this.cmbAño);
@@ -173,7 +255,14 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -193,6 +282,12 @@
         private System.Windows.Forms.ComboBox cmbAño;
         private System.Windows.Forms.ComboBox cmbDía;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
     }
 }
 
--- Pantalla 1 Registro/Form1.cs
+++ Pantalla 1 Registro/Form1.cs
@@ -16,5 +16,10 @@ namespace Pantalla_1_Registro
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
