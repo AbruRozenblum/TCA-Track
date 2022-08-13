2dfca6ee8ba5360f37d966803eb8e4bbe0a4106c
AbruRozenblum <110607863+AbruRozenblum@users.noreply.github.com>
2022-08-13 12:09:05 -0300

    num1 bienvenidoss

--- Pantalla 1 Registro/Program.cs
+++ Pantalla 1 Registro/Program.cs
@@ -16,7 +16,7 @@ namespace Pantalla_1_Registro
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Bienvenido());
         }
     }
 }
