abfe1dbeb793055401dd92047bb4b06300822f63
AbruRozenblum <110607863+AbruRozenblum@users.noreply.github.com>
2022-08-17 16:27:06 -0300

    lihyuktyft

--- Pantalla 1 Registro/Dulces.Designer.cs
+++ Pantalla 1 Registro/Dulces.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(995, 484);
+            this.ClientSize = new System.Drawing.Size(995, 520);
             this.Name = "Dulces";
             this.Text = "Dulces";
             this.ResumeLayout(false);
